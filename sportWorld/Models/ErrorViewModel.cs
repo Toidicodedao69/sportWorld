fedd0cdbb881b200b36c52abee578e2a3e846273
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-02-06 16:26:04 +0700

    Add project files.

---
+++ sportWorld/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace sportWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
