e80da050b563f9cc1a237bda87db1d0dfa998d18
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-02-07 23:24:27 +0700

    Create Category Model

---
+++ sportWorld/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sportWorld.Models
+{
+    public class Category
+    {
+
+        [Key] // Primary Key
+        public int Id { get; set; }
+        [Required] // Not null
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
