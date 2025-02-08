387d5a1bf9dbd5f26499612db53a2796ca5603eb
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-02-09 00:35:03 +0700

    Get, fetch, and display all data from Category table

--- sportWorld/Controllers/CategoryController.cs
+++ sportWorld/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using sportWorld.Data;
+using sportWorld.Models;
 
 namespace sportWorld.Controllers
 {
     public class CategoryController : Controller
     {
+        private ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> CategoryList = _db.Categories.ToList();
+            return View(CategoryList);
         }
     }
 }
