62a4e0bea3acc4e9a33a2b76781681b319bf1808
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-02-12 01:03:25 +0700

    Implement CategoryRepository

--- sportWorld.DataAccess/Repository/IRepository/IRepository.cs
+++ sportWorld.DataAccess/Repository/IRepository/IRepository.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace sportWorld.DataAccess.Repository.IRepository
 {
-	internal interface IRepository <T> where T : class
+	public interface IRepository <T> where T : class
 	{
 		// Generic methods to access db
 		IEnumerable<T> GetAll();
