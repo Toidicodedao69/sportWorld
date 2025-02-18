e4912526a0d521af618a45b51cdda40b6502aee9
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-02-19 01:03:35 +0700

    Add Delete API Use SweetAlert 2 for delete alert

--- sportWorld/Areas/Admin/Controllers/ProductController.cs
+++ sportWorld/Areas/Admin/Controllers/ProductController.cs
@@ -105,35 +105,35 @@ namespace sportWorld.Areas.Admin.Controllers
 			}
 		}
 		
-		public IActionResult Delete(int? id)
-		{
-			if (id == null || id == 0)
-			{
-				return NotFound();
-			}
-			Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
+		//public IActionResult Delete(int? id)
+		//{
+		//	if (id == null || id == 0)
+		//	{
+		//		return NotFound();
+		//	}
+		//	Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
 
-			if (productFromDb == null)
-			{
-				return NotFound();
-			}
-			return View(productFromDb);
-		}
-		[HttpPost, ActionName("Delete")] // Specify actions when the form is submitted 
-		public IActionResult DeletePOST(int? id) // This action method's name is still "Delete" as specified in ActionName annotation
-		{
-			Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
+		//	if (productFromDb == null)
+		//	{
+		//		return NotFound();
+		//	}
+		//	return View(productFromDb);
+		//}
+		//[HttpPost, ActionName("Delete")] // Specify actions when the form is submitted 
+		//public IActionResult DeletePOST(int? id) // This action method's name is still "Delete" as specified in ActionName annotation
+		//{
+		//	Product? obj = _unitOfWork.Product.Get(u => u.Id == id);
 
-			if (obj == null)
-			{
-				return NotFound();
-			}
+		//	if (obj == null)
+		//	{
+		//		return NotFound();
+		//	}
 
-			_unitOfWork.Product.Remove(obj);
-			_unitOfWork.Save();
-			TempData["success"] = "Product deleted successfully!"; // Show notification for only 1 render
-			return RedirectToAction("Index");
-		}
+		//	_unitOfWork.Product.Remove(obj);
+		//	_unitOfWork.Save();
+		//	TempData["success"] = "Product deleted successfully!"; // Show notification for only 1 render
+		//	return RedirectToAction("Index");
+		//}
 
 		#region API CALLS
 		[HttpGet]
@@ -143,6 +143,28 @@ namespace sportWorld.Areas.Admin.Controllers
 
             return Json(new { data = ProductList });
         }
+		[HttpDelete]
+		public IActionResult Delete(int? id)
+		{
+			var product = _unitOfWork.Product.Get(u => u.Id == id);
+
+			if (product == null)
+			{
+				return Json(new {success = false, message = "Error while deleting"});
+			}
+
+			var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, product.ImageUrl.TrimStart('\\'));
+
+			if (System.IO.File.Exists(oldImagePath))
+			{
+				System.IO.File.Delete(oldImagePath);
+			}
+
+			_unitOfWork.Product.Remove(product);
+			_unitOfWork.Save();
+
+			return Json(new {success = true, message = "Product deleted successfully!"});
+		}
         #endregion
     }
 }
