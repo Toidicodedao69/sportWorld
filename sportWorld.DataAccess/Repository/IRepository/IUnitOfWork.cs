138f0b1d47fe6ad9aabc8b84b42c88a8f3dd0faa
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-03-01 20:43:31 +0700

    Create Company Model Add CompanyRepo and add to IUnitofWork

--- sportWorld.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ sportWorld.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -10,7 +10,7 @@ namespace sportWorld.DataAccess.Repository.IRepository
 	{
 		ICategoryRepository Category { get; }
         IProductRepository Product { get; }
-
+		ICompanyRepository Company { get; }
         void Save();
 
 	}
