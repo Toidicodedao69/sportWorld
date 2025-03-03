1ae3bf464a716399df0b95fbc4b6115e6260c7d4
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-03-03 14:03:49 +0700

    Add ShoppingCart Model and Repo Add ApplicationUser Repo

---
+++ sportWorld.DataAccess/Repository/ShoppingCartRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using sportWorld.DataAccess.Data;
+using sportWorld.DataAccess.Repository.IRepository;
+using sportWorld.Models;
+
+
+namespace sportWorld.DataAccess.Repository
+{
+	public class ShoppingCartRepository : Repository<ShoppingCart>, IShoppingCartRepository
+	{
+		private ApplicationDbContext _db;
+        public ShoppingCartRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+		public void Update(ShoppingCart shoppingcart)
+		{
+			_db.ShoppingCarts.Update(shoppingcart);
+		}
+	}
+}
