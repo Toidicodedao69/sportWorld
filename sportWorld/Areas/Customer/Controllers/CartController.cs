9b477e65a00225a70d3eb2dead9b4d4b4509adad
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-03-04 13:16:16 +0700

    Add Stripe connection

--- sportWorld/Areas/Customer/Controllers/CartController.cs
+++ sportWorld/Areas/Customer/Controllers/CartController.cs
@@ -4,6 +4,7 @@ using Microsoft.Identity.Client;
 using sportWorld.DataAccess.Repository.IRepository;
 using sportWorld.Models;
 using sportWorld.Models.ViewModels;
+using sportWorld.Utility;
 using System.Net.Quic;
 using System.Security.Claims;
 
@@ -14,6 +15,8 @@ namespace sportWorld.Areas.Customer.Controllers
 	public class CartController : Controller
     {
         private readonly IUnitOfWork _unitOfWork;
+		[BindProperty] // Keep properties for all action methods after populating -> Only needs to populate once
+		public ShoppingCartVM cartVM { get; set; }
 		
 		public CartController(IUnitOfWork unitOfWork)
 		{
@@ -24,7 +27,7 @@ namespace sportWorld.Areas.Customer.Controllers
 			var claimsIdentity = (ClaimsIdentity)User.Identity;
 			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-			ShoppingCartVM cartVM = new()
+			cartVM = new()
 			{
 				CartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product").ToList(),
 				OrderHeader = new()
@@ -67,6 +70,66 @@ namespace sportWorld.Areas.Customer.Controllers
 
 			return View(cartVM);
 		}
+		[HttpPost]
+		[ActionName("OrderSummary")]
+		public IActionResult OrderSummaryPOST()
+		{
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+			cartVM.CartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId, includeProperties: "Product").ToList();
+
+			cartVM.OrderHeader.ApplicationUserId = userId;
+			cartVM.OrderHeader.OrderDate = System.DateTime.Now;
+
+			// DO NOT populate cartVM.OrderHeader.applicationUser
+			// Because EF Core will duplicate the record of applicationUser when adding OrderHeader to db
+			ApplicationUser applicationUser = _unitOfWork.ApplicationUser.Get(u => u.Id == userId);
+			
+			if (applicationUser.CompanyId.GetValueOrDefault() == 0)
+			{
+				// Customer user -> pending payment
+				cartVM.OrderHeader.OrderStatus = SD.StatusPending;
+				cartVM.OrderHeader.PaymentStatus = SD.PaymentStatusPending;
+			}
+			else
+			{
+				// Company user -> delayed payment
+				cartVM.OrderHeader.OrderStatus = SD.StatusApproved;
+				cartVM.OrderHeader.PaymentStatus = SD.PaymentStatusDelayedPayment;
+			}
+
+			foreach (var cart in cartVM.CartList)
+			{
+				cart.Price = GetPriceBasedOnQuantity(cart);
+				cartVM.OrderHeader.OrderTotal += cart.Price * cart.Count;
+			}
+
+			_unitOfWork.OrderHeader.Add(cartVM.OrderHeader); 
+			_unitOfWork.Save(); // Add OrderHeader before adding OrderDetail
+
+			foreach(var cart in cartVM.CartList)
+			{
+				OrderDetail orderDetail = new()
+				{
+					OrderHeaderId = cartVM.OrderHeader.Id, // Can access orderHeaderId because it was added when calling _unitOfWork.Save()
+					ProductId = cart.ProductId,
+					Count = cart.Count,
+					Price = cart.Price
+				};
+
+				_unitOfWork.OrderDetail.Add(orderDetail);
+			}
+
+			_unitOfWork.Save();
+
+			return RedirectToAction(nameof(OrderConfirmation), new { OrderHeaderId = cartVM.OrderHeader.Id });
+		}
+		public IActionResult OrderConfirmation(int OrderHeaderId)
+		{
+			return View(OrderHeaderId);
+		}
+		
 		public IActionResult Plus(int cartId)
 		{
 			var cart = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
