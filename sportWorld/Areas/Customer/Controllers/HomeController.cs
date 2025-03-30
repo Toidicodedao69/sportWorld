c46311854e583b1eb22cf103acb3c115b0c102b5
Nghia Hieu Pham <nghiahieuhn@gmail.com>
2025-03-30 18:11:14 +0700

    Add search for product name in home page

--- sportWorld/Areas/Customer/Controllers/HomeController.cs
+++ sportWorld/Areas/Customer/Controllers/HomeController.cs
@@ -8,74 +8,83 @@ using System.Security.Claims;
 
 namespace sportWorld.Areas.Customer.Controllers
 {
-    [Area("Customer")] // Associate the controller with the area
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly IUnitOfWork _unitOfWork;
-        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
-        {
-            _logger = logger;
-            _unitOfWork = unitOfWork;
-        }
+	[Area("Customer")] // Associate the controller with the area
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly IUnitOfWork _unitOfWork;
+		public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+		{
+			_logger = logger;
+			_unitOfWork = unitOfWork;
+		}
 
-        public IActionResult Index()
-        {
-			IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
-            return View(productList);
-        }
+		public IActionResult Index(string productName)
+		{
+			IEnumerable<Product> productList;
+
+			if (!String.IsNullOrEmpty(productName))
+			{
+				productList = _unitOfWork.Product.GetAll(u => u.Name.ToLower().Contains(productName.ToLower()), includeProperties: "Category");
+			} else
+			{
+				productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+			}
+
+			return View(productList);
+		}
 		public IActionResult Details(int productId)
 		{
-            ShoppingCart cart = new ()
-            {
-                Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
-                Count = 1,
-                ProductId = productId
-		    };
+			ShoppingCart cart = new()
+			{
+				Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category"),
+				Count = 1,
+				ProductId = productId
+			};
 
 			return View(cart);
 		}
-        [HttpPost]
-        [Authorize] // Must log in to add products
+		[HttpPost]
+		[Authorize] // Must log in to add products
 		public IActionResult Details(ShoppingCart cart)
 		{
-            var claimsIdentity = (ClaimsIdentity)User.Identity;
-            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
-            cart.ApplicationUserId = userId;
+			cart.ApplicationUserId = userId;
 
-            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId && u.ProductId == cart.ProductId);
+			ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId && u.ProductId == cart.ProductId);
 
-            if (cartFromDb != null)
-            {
-                // Update cart
-                cartFromDb.Count += cart.Count;
-                _unitOfWork.ShoppingCart.Update(cartFromDb);
+			if (cartFromDb != null)
+			{
+				// Update cart
+				cartFromDb.Count += cart.Count;
+				_unitOfWork.ShoppingCart.Update(cartFromDb);
 				_unitOfWork.Save();
 			}
 			else
-            {
+			{
 				// Add new cart
 				_unitOfWork.ShoppingCart.Add(cart);
 				_unitOfWork.Save();
-                // Set number of unique products in shopping cart
+				// Set number of unique products in shopping cart
 				HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
 			}
 
-            TempData["success"] = "Cart updated successfully";
+			TempData["success"] = "Cart updated successfully";
 
 			return RedirectToAction(nameof(Index));     // nameof find all Action pages in the current Controller
 		}
 
 		public IActionResult Privacy()
-        {
-            return View();
-        }
+		{
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
